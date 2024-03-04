a3ecbd62f38f0f3ba48a1be911258017a56f1394
nelson <nelsonagbagah1002@gmail.com>
2024-03-04 04:45:01 -0800

    venv
    added composite.py
    
    added requirement.txt

---
+++ composite.py
@@ -0,0 +1,27 @@
+import sympy as sp
+x = sp.Symbol('x')
+
+def f(x):
+    return 2 * x + 1
+
+def g(x):
+    return 3*x + 5
+
+
+def composite_func(f, g ,x):
+    return f(g(x))
+
+
+
+if __name__ == "__main__":
+    
+
+    result = composite_func(f,g,x)
+    print("equation of h(x)-----------------------")
+    print('h(x) = ' + str(result))
+    x_val = float(input("Enter the value of x: "))
+    value = composite_func(f, g, x_val)
+    print('h(x) = ' + str(value))
+    print()
+
+
